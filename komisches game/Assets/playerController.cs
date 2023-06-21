9453bd3571ffb1e0837fd760cf1166d4bbd37864
kampfkatoffel17 <118006846+kampfkatoffel17@users.noreply.github.com>
2023-06-21 15:23:13 +0200

    movement basic

---
+++ komisches game/Assets/playerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed = 6f;
+
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+}
